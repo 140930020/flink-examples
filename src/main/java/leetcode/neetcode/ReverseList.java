f5687b13cace420e18efb27d5738071f9aa8d135
tangqiuyue@jecn.com.cn <tangqiuyue@jecn.com.cn>
2022-08-19 10:13:47 +0800

    [lc-203] 删除链表元素

--- src/main/java/leetcode/neetcode/ReverseList.java
+++ src/main/java/leetcode/neetcode/ReverseList.java
@@ -5,9 +5,9 @@ package leetcode.neetcode;
  * @author 2022/8/18
  */
 public class ReverseList {
-    class ListNode {
+    static class ListNode {
         int val;
-        leetcode.neetcode.ListNode next;
+        ListNode next;
 
         ListNode() {
         }
@@ -16,25 +16,64 @@ public class ReverseList {
             this.val = val;
         }
 
-        ListNode(int val, leetcode.neetcode.ListNode next) {
+        ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
 
 
-        class Solution {
-            public leetcode.neetcode.ListNode reverseList(leetcode.neetcode.ListNode head) {
-                leetcode.neetcode.ListNode prev = null, curr = head;
+        static class Solution {
+            public ListNode reverseList(ListNode head) {
+                ListNode prev = null, curr = head;
                 while (curr != null) {
-                    leetcode.neetcode.ListNode nextNode = curr.next;
+                    ListNode nextNode = curr.next;
                     curr.next = prev;
                     prev = curr;
                     curr = nextNode;
                 }
                 return prev;
             }
+
+            /**
+             * null-1-2-6-5-4
+             *
+             * @param head
+             * @param val
+             * @return
+             */
+            public ListNode removeRElements(ListNode head, int val) {
+                ListNode dummy = new ListNode();
+                dummy.next = head;
+                ListNode curr = head, prev = dummy;
+                while (curr != null) {
+                    if (curr.val == val) {
+                        prev.next = curr.next; //改变之前的next指针
+                    } else {
+                        prev = curr;
+                    }
+                    curr = curr.next;
+                }
+                return dummy.next;
+            }
         }
     }
 
+    public static void main(String[] args) {
+        ReverseList.ListNode.Solution solution = new ReverseList.ListNode.Solution();
+        ListNode listNode = new ListNode(7);
+        addHead(listNode, 7);
+        addHead(listNode, 7);
+        addHead(listNode, 7);
+        solution.removeRElements(listNode, 7);
+    }
+
+    private static ListNode addHead(ListNode listNode, int val) {
+        ListNode curr = listNode;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = new ListNode(val);
+        return listNode.next;
+    }
 
 }
